805bd8c93140c2ced24693ba8814049b2f2f314a
elenadimitrova <elenavdimitrova@gmail.com>
2018-04-15 18:16:36 +0300

    Add token and tests from colonyNetwork repo Start testing contracts in CI git ignore the test-accounts generated file

---
+++ helpers/test-helper.js
@@ -0,0 +1,176 @@
+/* eslint-disable no-console */
+import shortid from "shortid";
+import { assert } from "chai";
+
+export function web3GetNetwork() {
+  return new Promise((resolve, reject) => {
+    web3.version.getNetwork((err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+export function web3GetClient() {
+  return new Promise((resolve, reject) => {
+    web3.version.getNode((err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+export function web3GetBalance(account) {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBalance(account, (err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+export function web3GetTransaction(txid) {
+  return new Promise((resolve, reject) => {
+    web3.eth.getTransaction(txid, (err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+export function web3GetTransactionReceipt(txid) {
+  return new Promise((resolve, reject) => {
+    web3.eth.getTransactionReceipt(txid, (err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+export function web3GetFirstTransactionHashFromLastBlock() {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBlock("latest", true, (err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res.transactions[0].hash);
+    });
+  });
+}
+
+export function web3GetCode(a) {
+  return new Promise((resolve, reject) => {
+    web3.eth.getCode(a, (err, res) => {
+      if (err !== null) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+async function checkError(promise, isAssert) {
+  // There is a discrepancy between how ganache-cli handles errors
+  // (throwing an exception all the way up to these tests) and how geth/parity handle them
+  // (still making a valid transaction and returning a txid). For the explanation of why
+  // See https://github.com/ethereumjs/testrpc/issues/39
+  //
+  // Obviously, we want our tests to pass on all, so this is a bit of a problem.
+  // We have to have this special function that we use to catch the error.
+  let tx;
+  let receipt;
+  try {
+    tx = await promise;
+    receipt = await web3GetTransactionReceipt(tx);
+  } catch (err) {
+    ({ tx, receipt } = err);
+  }
+
+  // Check the receipt `status` to ensure transaction failed.
+  assert.equal(receipt.status, 0x00);
+
+  if (isAssert) {
+    const network = await web3GetNetwork();
+    const transaction = await web3GetTransaction(tx);
+    if (network !== "coverage") {
+      // When a transaction `throws`, all the gas sent is spent. So let's check that we spent all the gas that we sent.
+      // When using EtherRouter not all sent gas is spent, it is 73000 gas less than the total.
+      assert.closeTo(transaction.gas, receipt.gasUsed, 73000, "didnt fail - didn't throw and use all gas");
+    }
+  }
+}
+
+export async function checkErrorRevert(promise) {
+  return checkError(promise, false);
+}
+
+export async function checkErrorAssert(promise) {
+  return checkError(promise, true);
+}
+
+export function checkErrorNonPayableFunction(tx) {
+  assert.equal(tx, "Error: Cannot send value to non-payable function");
+}
+
+export function getRandomString(_length) {
+  const length = _length || 7;
+  let randString = "";
+  while (randString.length < length) {
+    randString += shortid
+      .generate()
+      .replace(/_/g, "")
+      .toLowerCase();
+  }
+  return randString.slice(0, length);
+}
+
+export function getTokenArgs() {
+  return [getRandomString(5), getRandomString(3), 18];
+}
+
+export async function currentBlockTime() {
+  const p = new Promise((resolve, reject) => {
+    web3.eth.getBlock("latest", (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(res.timestamp);
+    });
+  });
+  return p;
+}
+
+export async function getBlockTime(blockNumber) {
+  const p = new Promise((resolve, reject) => {
+    web3.eth.getBlock(blockNumber, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(res.timestamp);
+    });
+  });
+  return p;
+}
+
+export async function expectEvent(tx, eventName) {
+  const { logs } = await tx;
+  const event = logs.find(e => e.event === eventName);
+  return assert.exists(event);
+}
+
+export async function forwardTime(seconds, test) {
+  const client = await web3GetClient();
+  if (client.indexOf("TestRPC") === -1) {
+    test.skip();
+  } else {
+    // console.log(`Forwarding time with ${seconds}s ...`);
+    await web3.currentProvider.send({
+      jsonrpc: "2.0",
+      method: "evm_increaseTime",
+      params: [seconds],
+      id: 0
+    });
+    await web3.currentProvider.send({
+      jsonrpc: "2.0",
+      method: "evm_mine",
+      params: [],
+      id: 0
+    });
+  }
+}
---
+++ test/token.js
@@ -0,0 +1,156 @@
+/* globals artifacts */
+
+import { assert } from "chai";
+import { expectEvent, checkErrorAssert, checkErrorRevert, web3GetBalance, checkErrorNonPayableFunction } from "../helpers/test-helper";
+
+const Token = artifacts.require("Token");
+
+contract("Token", accounts => {
+  const COINBASE_ACCOUNT = accounts[0];
+  const ACCOUNT_TWO = accounts[1];
+  const ACCOUNT_THREE = accounts[2];
+
+  let token;
+
+  beforeEach(async () => {
+    token = await Token.new("Colony token", "CLNY", 18);
+  });
+
+  describe("when working with ERC20 functions", () => {
+    beforeEach("mint 1500000 tokens", async () => {
+      await token.mint(1500000);
+    });
+
+    it("should be able to get total supply", async () => {
+      const total = await token.totalSupply.call();
+      assert.equal(1500000, total.toNumber());
+    });
+
+    it("should be able to get token balance", async () => {
+      const balance = await token.balanceOf.call(COINBASE_ACCOUNT);
+      assert.equal(1500000, balance.toNumber());
+    });
+
+    it("should be able to get allowance for address", async () => {
+      await token.approve(ACCOUNT_TWO, 200000);
+      const allowance = await token.allowance.call(COINBASE_ACCOUNT, ACCOUNT_TWO);
+      assert.equal(200000, allowance.toNumber());
+    });
+
+    it("should be able to transfer tokens from own address", async () => {
+      const success = await token.transfer.call(ACCOUNT_TWO, 300000);
+      assert.equal(true, success);
+
+      await expectEvent(token.transfer(ACCOUNT_TWO, 300000), "Transfer");
+      const balanceAccount1 = await token.balanceOf.call(COINBASE_ACCOUNT);
+      assert.equal(1200000, balanceAccount1.toNumber());
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(300000, balanceAccount2.toNumber());
+    });
+
+    it("should NOT be able to transfer more tokens than they have", async () => {
+      await checkErrorAssert(token.transfer(ACCOUNT_TWO, 1500001));
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(0, balanceAccount2.toNumber());
+    });
+
+    it("should be able to transfer pre-approved tokens from address different than own", async () => {
+      await token.approve(ACCOUNT_TWO, 300000);
+      const success = await token.transferFrom.call(COINBASE_ACCOUNT, ACCOUNT_TWO, 300000, { from: ACCOUNT_TWO });
+      assert.equal(true, success);
+
+      await expectEvent(token.transferFrom(COINBASE_ACCOUNT, ACCOUNT_TWO, 300000, { from: ACCOUNT_TWO }), "Transfer");
+      const balanceAccount1 = await token.balanceOf.call(COINBASE_ACCOUNT);
+      assert.equal(1200000, balanceAccount1.toNumber());
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(300000, balanceAccount2.toNumber());
+      const allowance = await token.allowance.call(COINBASE_ACCOUNT, ACCOUNT_TWO);
+      assert.equal(0, allowance.toNumber());
+    });
+
+    it("should NOT be able to transfer tokens from another address if NOT pre-approved", async () => {
+      await checkErrorAssert(token.transferFrom(COINBASE_ACCOUNT, ACCOUNT_TWO, 300000, { from: ACCOUNT_TWO }));
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(0, balanceAccount2.toNumber());
+    });
+
+    it("should NOT be able to transfer from another address more tokens than pre-approved", async () => {
+      await token.approve(ACCOUNT_TWO, 300000);
+      await checkErrorAssert(token.transferFrom(COINBASE_ACCOUNT, ACCOUNT_TWO, 300001, { from: ACCOUNT_TWO }));
+
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(0, balanceAccount2.toNumber());
+    });
+
+    it("should NOT be able to transfer from another address more tokens than the source balance", async () => {
+      await token.approve(ACCOUNT_TWO, 300000);
+      await token.transfer(ACCOUNT_THREE, 1500000);
+
+      await checkErrorAssert(token.transferFrom(COINBASE_ACCOUNT, ACCOUNT_TWO, 300000, { from: ACCOUNT_TWO }));
+      const balanceAccount2 = await token.balanceOf.call(ACCOUNT_TWO);
+      assert.equal(0, balanceAccount2.toNumber());
+    });
+
+    it("should be able to approve token transfer for other accounts", async () => {
+      const success = await token.approve.call(ACCOUNT_TWO, 200000);
+      assert.equal(true, success);
+
+      await expectEvent(token.approve(ACCOUNT_TWO, 200000), "Approval");
+      const allowance = await token.allowance.call(COINBASE_ACCOUNT, ACCOUNT_TWO);
+      assert.equal(200000, allowance.toNumber());
+    });
+  });
+
+  describe("when working with additional functions", () => {
+    it("should be able to mint new tokens, when called by the Token owner", async () => {
+      await token.mint(1500000, { from: COINBASE_ACCOUNT });
+      let totalSupply = await token.totalSupply.call();
+      assert.equal(1500000, totalSupply.toNumber());
+
+      let balance = await token.balanceOf.call(COINBASE_ACCOUNT);
+      assert.equal(1500000, balance.toNumber());
+
+      // Mint some more tokens
+      await token.mint(1);
+      totalSupply = await token.totalSupply.call();
+      assert.equal(1500001, totalSupply.toNumber());
+
+      balance = await token.balanceOf.call(COINBASE_ACCOUNT);
+      assert.equal(1500001, balance.toNumber());
+    });
+
+    it("should NOT be able to mint new tokens, when called by anyone NOT the Token owner", async () => {
+      await checkErrorRevert(token.mint(1500000, { from: ACCOUNT_THREE }));
+      const totalSupply = await token.totalSupply.call();
+      assert.equal(0, totalSupply.toNumber());
+    });
+  });
+
+  describe("when working with ether transfers", () => {
+    it("should NOT accept eth", async () => {
+      await checkErrorRevert(token.send(2));
+      const tokenBalance = await web3GetBalance(token.address);
+      assert.equal(0, tokenBalance.toNumber());
+    });
+
+    it.skip("should NOT accept eth via etherRouter transfer", async () => {
+      await checkErrorRevert(await token.send(2));
+      const tokenBalance = await web3GetBalance(token.address);
+      assert.equal(0, tokenBalance.toNumber());
+    });
+
+    it("should NOT accept eth via etherRouter call to function", async () => {
+      try {
+        await token.mint(200, { value: 2 });
+      } catch (err) {
+        checkErrorNonPayableFunction(err);
+      }
+
+      const totalSupply = await token.totalSupply.call();
+      assert.equal(0, totalSupply.toNumber());
+
+      const tokenBalance = await web3GetBalance(token.address);
+      assert.equal(0, tokenBalance.toNumber());
+    });
+  });
+});
