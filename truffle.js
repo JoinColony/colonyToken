e16f6d68a47c264b2f44a9e91ce5b8b5f6125e31
Daniel Kronovet <kronovet@gmail.com>
2018-08-21 12:02:09 +0300

    Make repo into EPM package

--- truffle.js
+++ truffle.js
@@ -4,6 +4,8 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const mnemonic = "";
 
+const API_KEY = "OGfF4xYJ82HoyaJNFkla";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -13,16 +15,22 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    coverage: {
-      host: "127.0.0.1",
-      port: 8555,
-      network_id: "*" // Match any network id
-    },
-    kovan: {
+    // coverage: {
+    //   host: "127.0.0.1",
+    //   port: 8555,
+    //   network_id: "*" // Match any network id
+    // },
+    // kovan: {
+    //   provider() {
+    //     return new HDWalletProvider(mnemonic, `https://kovan.infura.io/${API_KEY}`);
+    //   },
+    //   network_id: "42"
+    // },
+    ropsten: {
       provider() {
-        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/OGfF4xYJ82HoyaJNFkla");
+        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${API_KEY}`);
       },
-      network_id: "42"
+      network_id: "3"
     }
   },
   mocha: {
