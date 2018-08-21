74ea5632691339a56ad329dbb73e610927f87524
elenadimitrova <elenavdimitrova@gmail.com>
2018-08-21 14:54:55 +0300

    Upgrade truffle to @next including fixing breaking changes for: -Replace getData with encodeABI syntax -Remove .call() syntax from tests -Await .at executions -Work with bn.js instead of BigNumber -changes associated with move to web3 v1
    Also fixed an issue in vesting tests where for a claim done after the
    entire grant had vested, we calculated partial amounts instead of
    expecting the entire grant amount to be claimed.
    
    Additionally upgrades all other packages to latest.

--- helpers/test-helper.js
+++ helpers/test-helper.js
@@ -4,7 +4,7 @@ import { assert } from "chai";
 
 export function web3GetNetwork() {
   return new Promise((resolve, reject) => {
-    web3.version.getNetwork((err, res) => {
+    web3.eth.net.getId((err, res) => {
       if (err !== null) return reject(err);
       return resolve(res);
     });
@@ -13,7 +13,7 @@ export function web3GetNetwork() {
 
 export function web3GetClient() {
   return new Promise((resolve, reject) => {
-    web3.version.getNode((err, res) => {
+    web3.eth.getNodeInfo((err, res) => {
       if (err !== null) return reject(err);
       return resolve(res);
     });
@@ -156,21 +156,39 @@ export async function expectEvent(tx, eventName) {
 
 export async function forwardTime(seconds, test) {
   const client = await web3GetClient();
-  if (client.indexOf("TestRPC") === -1) {
-    test.skip();
-  } else {
-    console.log(`Forwarding time with ${seconds}s ...`);
-    await web3.currentProvider.send({
-      jsonrpc: "2.0",
-      method: "evm_increaseTime",
-      params: [seconds],
-      id: 0
-    });
-    await web3.currentProvider.send({
-      jsonrpc: "2.0",
-      method: "evm_mine",
-      params: [],
-      id: 0
-    });
-  }
+  const p = new Promise((resolve, reject) => {
+    if (client.indexOf("TestRPC") === -1) {
+      resolve(test.skip());
+    } else {
+      console.log(`Forwarding time with ${seconds}s ...`);
+      web3.currentProvider.send(
+        {
+          jsonrpc: "2.0",
+          method: "evm_increaseTime",
+          params: [seconds],
+          id: 0
+        },
+        err => {
+          if (err) {
+            return reject(err);
+          }
+          return web3.currentProvider.send(
+            {
+              jsonrpc: "2.0",
+              method: "evm_mine",
+              params: [],
+              id: 0
+            },
+            (err2, res) => {
+              if (err2) {
+                return reject(err2);
+              }
+              return resolve(res);
+            }
+          );
+        }
+      );
+    }
+  });
+  return p;
 }
