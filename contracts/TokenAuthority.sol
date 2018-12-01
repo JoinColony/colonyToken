7359eedaadacd55a1393c795964bd61513b2af33
elenadimitrova <elenavdimitrova@gmail.com>
2018-12-02 00:03:29 +0200

    Add an array of reputation miners who are allowed to transfer CLNY

--- contracts/TokenAuthority.sol
+++ contracts/TokenAuthority.sol
@@ -24,7 +24,13 @@ contract TokenAuthority is DSAuthority {
   address public token;
   mapping(address => mapping(bytes4 => bool)) authorizations;
   
-  constructor(address _token, address _colonyNetwork, address _metaColony, address _tokenLocking, address _vesting) public {
+  constructor(
+    address _token,
+    address _colonyNetwork,
+    address _metaColony,
+    address _tokenLocking,
+    address _vesting,
+    address[] miners) public {
     token = _token;
     bytes4 transferSig = bytes4(keccak256("transfer(address,uint256)"));
     bytes4 transferFromSig = bytes4(keccak256("transferFrom(address,address,uint256)"));
@@ -42,6 +48,12 @@ contract TokenAuthority is DSAuthority {
 
     authorizations[_vesting][transferSig] = true;
     authorizations[_vesting][transferFromSig] = true;
+
+    // Allow passing in of multiple reputation miner accounts although potentially one will be used in production
+    for (uint i = 0; i < miners.length; i++) {
+      address miner = miners[i];
+      authorizations[miner][transferSig] = true;
+    }
   }
 
   function canCall(address src, address dst, bytes4 sig) public view returns (bool) {
