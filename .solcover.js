be498512a89f8e82ae0e93f2f3ab1844b64bb01b
elenadimitrova <elenavdimitrova@gmail.com>
2019-03-20 09:17:27 +0000

    Upgrade solidity-coverage and fix compilation

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,7 @@ module.exports = {
       'Migrations.sol',
       'gnosis'
     ],
-    compileCommand: '../node_modules/.bin/truffle compile',
+    compileCommand: 'yarn run provision:multisig:contract',
     testCommand: '../node_modules/.bin/truffle test --network coverage',
     testrpcOptions: `--port 8555 -i 1999 --acctKeys="./coverageEnv/ganache-accounts.json" --noVMErrorsOnRPCResponse --accounts 12`
 };
