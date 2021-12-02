3b5a4b5a670625afdafa6deda85ed2dc9f7f35d6
Daniel Kronovet <kronovet@gmail.com>
2021-12-02 18:30:27 -0500

    Update contract and compiler versions to 0.7.3

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,5 @@
 
-pragma solidity >=0.5.3;
+pragma solidity >=0.7.3;
 
 contract Migrations {
   address public owner;
--- truffle.js
+++ truffle.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.5.8",
+      version: "0.7.3",
       docker: true,
       settings: {
         optimizer: {
